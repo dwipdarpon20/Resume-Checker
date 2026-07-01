5abca3ccc68ea31361777b57964ed5abc5fde010
Dwip Darpon Sarker <dwipdarpon0020@gmail.com>
2026-07-01 15:01:45 +0530

    Update axios baseURL for production environment

--- frontend/src/services/ai/interview.services.js
+++ frontend/src/services/ai/interview.services.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create ({
-    baseURL : 'http://localhost:8080/api/interview',
+    baseURL : 'https://resume-checker-axi7.onrender.com/interview',
     withCredentials : true
 });
 
